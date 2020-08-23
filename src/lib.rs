5ce515cec9e43cb42640905bde6b3112f0be4843
neverRare <35887047+neverRare@users.noreply.github.com>
2020-08-23 13:10:37 +0800

    format

--- src/lib.rs
+++ src/lib.rs
@@ -24,46 +24,101 @@ macro_rules! internal_simple_eval {
     // $active is a usize either 0 or 1
     // these should be an identifier
     (($stack:ident, $active:ident) ()) => { 0 };
-    (($stack:ident, $active:ident) (()$($code:tt)*)) => {
-        $crate::internal_simple_eval!(($stack, $active) ($($code)*)) + 1
-    };
-    (($stack:ident, $active:ident) ([]$($code:tt)*)) => {
-        $stack[$active].len() + $crate::internal_simple_eval!(($stack, $active) ($($code)*))
-    };
-    (($stack:ident, $active:ident) ({}$($code:tt)*)) => {
-        $stack[$active].pop().unwrap_or_default() + $crate::internal_simple_eval!(($stack, $active) ($($code)*))
-    };
+    (($stack:ident, $active:ident) (()$($code:tt)*)) => {{
+        let rest = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        };
+        rest + 1
+    }};
+    (($stack:ident, $active:ident) ([]$($code:tt)*)) => {{
+        let len = $stack[$active].len();
+        let rest = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        };
+        rest + len
+    }};
+    (($stack:ident, $active:ident) ({}$($code:tt)*)) => {{
+        let popped = $stack[$active].pop().unwrap_or_default();
+        let rest = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        };
+        rest + popped
+    }};
     (($stack:ident, $active:ident) (@()$($code:tt)*)) => {{
         $active = 1 - $active;
-        $crate::internal_simple_eval!(($stack, $active) ($($code)*))
+        $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        }
     }};
     (($stack:ident, $active:ident) (($($first:tt)+)$($code:tt)*)) => {{
         use core::convert::TryInto;
-        let num = $crate::internal_simple_eval!(($stack, $active) ($($first)+)).try_into().unwrap();
+        let num = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($first)+)
+        };
+        let num = num.try_into().unwrap();
         $stack[$active].push(num);
-        $crate::internal_simple_eval!(($stack, $active) ($($code)*)) + num
+        let rest = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        };
+        rest + num
     }};
-    (($stack:ident, $active:ident) ([$($first:tt)+]$($code:tt)*)) => {
-        -$crate::internal_simple_eval!(($stack, $active) ($($first)*)) + $crate::internal_simple_eval!(($stack, $active) ($($code)*))
-    };
+    (($stack:ident, $active:ident) ([$($first:tt)+]$($code:tt)*)) => {{
+        let num = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($first)*)
+        };
+        let rest = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        };
+        rest - num
+    }};
     (($stack:ident, $active:ident) ({$($first:tt)+}$($code:tt)*)) => {{
         let mut num = 0;
         while let Some(top) = $stack[$active].last() {
             if *top == 0 {
                 break;
             } else {
-                num += $crate::internal_simple_eval!(($stack, $active) ($($first)+));
+                num += $crate::internal_simple_eval! {
+                    ($stack, $active)
+                    ($($first)+)
+                };
             }
         }
-        num + $crate::internal_simple_eval!(($stack, $active) ($($code)*))
+        let rest = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        };
+        rest + num
     }};
     (($stack:ident, $active:ident) (@($($first:tt)+)$($code:tt)*)) => {{
-        $crate::internal_simple!(($stack, $active) ($($first)*));
-        $crate::internal_simple_eval!(($stack, $active) ($($code)*))
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($first)*)
+        }
+        $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        }
     }};
-    (($stack:ident, $active:ident) (!($($first:tt)+)$($code:tt)*)) => {
-        $crate::internal!(($stack, $active, internal_simple_eval) (()) ($($first)*)) + $crate::internal_simple_eval!(($stack, $active) ($($code)*))
-    };
+    (($stack:ident, $active:ident) (!($($first:tt)+)$($code:tt)*)) => {{
+        let num = $crate::internal! {
+            ($stack, $active, internal_simple_eval)
+            (())
+            ($($first)*)
+        };
+        let rest = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($code)*)
+        };
+        rest + num
+    }};
 }
 // same as above, but discards the return value as possible.
 // necessary to avoid "unused" warnings.
@@ -71,51 +126,91 @@ macro_rules! internal_simple_eval {
 #[macro_export]
 macro_rules! internal_simple {
     (($stack:ident, $active:ident) ()) => { () };
-    (($stack:ident, $active:ident) (()$($code:tt)*)) => {{
-        $crate::internal_simple!(($stack, $active) ($($code)*));
-    }};
-    (($stack:ident, $active:ident) ([]$($code:tt)*)) => {{
-        $crate::internal_simple!(($stack, $active) ($($code)*));
-    }};
+    (($stack:ident, $active:ident) (()$($code:tt)*)) => {
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($code)*)
+        }
+    };
+    (($stack:ident, $active:ident) ([]$($code:tt)*)) => {
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($code)*)
+        }
+    };
     (($stack:ident, $active:ident) ({}$($code:tt)*)) => {{
         $stack[$active].pop();
-        $crate::internal_simple!(($stack, $active) ($($code)*));
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($code)*)
+        }
     }};
     (($stack:ident, $active:ident) (@()$($code:tt)*)) => {{
         $active = 1 - $active;
-        $crate::internal_simple!(($stack, $active) ($($code)*));
-    }};
-    (($stack:ident, $active:ident) (<<>>$($code:tt)*)) => {{
-        $active = 1 - $active;
-        $crate::internal_simple!(($stack, $active) ($($code)*));
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($code)*)
+        }
     }};
     (($stack:ident, $active:ident) (($($first:tt)+)$($code:tt)*)) => {{
         use core::convert::TryInto;
-        let num = $crate::internal_simple_eval!(($stack, $active) ($($first)+)).try_into().unwrap();
+        let num = $crate::internal_simple_eval! {
+            ($stack, $active)
+            ($($first)+)
+        };
+        let num = num.try_into().unwrap();
         $stack[$active].push(num);
-        $crate::internal_simple!(($stack, $active) ($($code)*));
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($code)*)
+        }
     }};
     (($stack:ident, $active:ident) ([$($first:tt)+]$($code:tt)*)) => {{
-        $crate::internal_simple!(($stack, $active) ($($first)*));
-        $crate::internal_simple!(($stack, $active) ($($code)*));
+        $crate::internal_simple!{
+            ($stack, $active)
+            ($($first)*)
+        }
+        $crate::internal_simple!{
+            ($stack, $active)
+            ($($code)*)
+        }
     }};
     (($stack:ident, $active:ident) ({$($first:tt)+}$($code:tt)*)) => {{
         while let Some(num) = $stack[$active].last() {
             if *num == 0 {
                 break;
             } else {
-                $crate::internal_simple!(($stack, $active) ($($first)+));
+                $crate::internal_simple! {
+                    ($stack, $active)
+                    ($($first)+)
+                };
             }
         }
-        $crate::internal_simple!(($stack, $active) ($($code)*));
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($code)*)
+        };
     }};
     (($stack:ident, $active:ident) (@($($first:tt)+)$($code:tt)*)) => {{
-        $crate::internal_simple!(($stack, $active) ($($first)*));
-        $crate::internal_simple!(($stack, $active) ($($code)*));
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($first)*)
+        }
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($code)*)
+        }
     }};
     (($stack:ident, $active:ident) (!($($first:tt)+)$($code:tt)*)) => {{
-        $crate::internal!(($stack, $active, internal_simple) (()) ($($first)*));
-        $crate::internal_simple!(($stack, $active) ($($code)*));
+        $crate::internal! {
+            ($stack, $active, internal_simple)
+            (())
+            ($($first)*)
+        }
+        $crate::internal_simple! {
+            ($stack, $active)
+            ($($code)*)
+        }
     }};
 }
 // another brain flak macro that deals with <...>
@@ -125,48 +220,102 @@ macro_rules! internal_simple {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! internal {
-    // look how monstrous this is, soon I will make this more manageable
     (($stack:ident, $active:ident, $macro:ident) (($($first:tt)*)) ()) => {
-        $crate::$macro!(($stack, $active) ($($first)*))
+        $crate::$macro! {
+            ($stack, $active)
+            ($($first)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)$($rest:tt)*) (<<>>$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($first)*@())$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($first)*@())$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)($($second:tt)*)$($rest:tt)*) (<>>$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($second)*@($($first)*@()))$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($second)*@($($first)*@()))$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)$($rest:tt)*) (<>$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($first)*@())$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($first)*@())$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) ($($rest:tt)*) (<<$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (()()$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (()()$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) ($($rest:tt)*) (<$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (()$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (()$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)($($second:tt)*)($($third:tt)*)$($rest:tt)*) (>>$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($third)*@($($second)*@($($first)*)))$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($third)*@($($second)*@($($first)*)))$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)($($second:tt)*)$($rest:tt)*) (>$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($second)*@($($first)*))$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($second)*@($($first)*))$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)$($rest:tt)*) (()$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($first)*())$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($first)*())$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)$($rest:tt)*) ([]$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($first)*[])$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($first)*[])$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)$($rest:tt)*) ({}$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($first)*{})$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($first)*{})$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)$($rest:tt)*) (($($token:tt)+)$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($first)*(!($($token)+)))$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($first)*(!($($token)+)))$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)$($rest:tt)*) ([$($token:tt)+]$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($first)*[!($($token)+)])$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($first)*[!($($token)+)])$($rest)*)
+            ($($code)*)
+        }
     };
     (($($meta:tt)*) (($($first:tt)*)$($rest:tt)*) ({$($token:tt)+}$($code:tt)*)) => {
-        $crate::internal!(($($meta)*) (($($first)*{!($($token)+)})$($rest)*) ($($code)*))
+        $crate::internal! {
+            ($($meta)*)
+            (($($first)*{!($($token)+)})$($rest)*)
+            ($($code)*)
+        }
     };
 }
 /// Stack manipulation with [Brain-Flak](https://esolangs.org/wiki/Brain-Flak). Refer to the [crate document](./index.html) for more info.
@@ -178,16 +327,24 @@ macro_rules! brain_flak {
         let stacks = [left, right];
         #[allow(unused_mut)]
         let mut active = 0;
-        $crate::internal!((stacks, active, internal_simple) (()) ($($code)*));
+        $crate::internal! {
+            (stacks, active, internal_simple)
+            (())
+            ($($code)*)
+        }
     }};
     ($input:expr $(,)? => $($code:tt)*) => {{
         let mut right = vec![];
-        $crate::brain_flak!($input, &mut right => $($code)*);
+        $crate::brain_flak! { $input, &mut right =>
+            $($code)*
+        };
     }};
     ($(=>)? $($code:tt)*) => {{
         let mut left = vec![];
         let mut right = vec![];
-        $crate::brain_flak!(&mut left, &mut right => $($code)*);
+        $crate::brain_flak! { &mut left, &mut right =>
+            $($code)*
+        };
         left
     }};
 }
